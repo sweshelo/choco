af90053798153aeb911edbe939830ab3d9c7a9af
sweshelo <sweshelo@gmail.com>
2025-02-08 16:58:36 +0900

    style: 💄 ログ出力内容を調整

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import ranking from "./modules/ranking";
 
+const fetchRankingWithLogging = () => {
+  try {
+    console.log(new Date());
+    ranking().then(() => console.log('Completed.'));
+  } catch (e) {
+    console.log(new Date());
+    console.error(e);
+  };
+}
+
 export default function main() {
   console.log('HELL WORD 👹');
-  setInterval(() => {
-    ranking();
-  }, 1000 * 60 * 3);
+  console.log('CHOCO STARTED');
+  fetchRankingWithLogging();
+  setInterval(fetchRankingWithLogging, 1000 * 60 * 3);
 }
 
 main();
